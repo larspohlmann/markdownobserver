8f979e92f2b259191abf6feae269d8bc2a92df0e
Lars Pohlmann <lars.pohlmann.ext@statista.com>
2026-04-04 09:51:15 +0200

    refactor: make FileOpenPlan.SlotAssignment Equatable for test ergonomics

--- minimark/Services/FileOpenCoordinator.swift
+++ minimark/Services/FileOpenCoordinator.swift
@@ -44,7 +44,7 @@ struct FileOpenRequest {
 }
 
 struct FileOpenPlan {
-    struct SlotAssignment {
+    struct SlotAssignment: Equatable {
         let fileURL: URL
         let target: SlotTarget
         let loadMode: LoadMode
