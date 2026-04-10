0e7fcea7bd6784d0b61a5098edab09d36b28b29a
Lars Pohlmann <lars.pohlmann@googlemail.com>
2026-04-10 10:07:34 +0200

    Add FolderEventSource protocol and factory

---
+++ minimark/Services/FolderEventSource.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+protocol FolderEventSource: AnyObject, Sendable {
+    var recommendedSafetyPollingInterval: DispatchTimeInterval { get }
+
+    func start(
+        folderURL: URL,
+        includeSubfolders: Bool,
+        exclusionMatcher: FolderWatchExclusionMatcher,
+        queue: DispatchQueue,
+        onEvent: @escaping @Sendable (_ changedDirectoryURLs: Set<URL>?) -> Void
+    )
+
+    func stop()
+}
+
+enum FolderEventSourceFactory {
+    static func makeEventSource(includeSubfolders: Bool) -> any FolderEventSource {
+        if includeSubfolders {
+            return FSEventStreamFolderEventSource()
+        } else {
+            return DispatchSourceFolderEventSource()
+        }
+    }
+}
