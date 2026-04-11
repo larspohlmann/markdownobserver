675f0a20b2406b609397ce8c4bc9010851af3175
Lars Pohlmann <lars.pohlmann@googlemail.com>
2026-04-11 18:13:51 +0200

    feat: add DocumentContent struct for observation split (#245)

---
+++ minimark/Models/DocumentContent.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct DocumentContent {
+    var savedMarkdown: String = ""
+    var sourceMarkdown: String = ""
+    var renderedHTMLDocument: String = ""
+    var changedRegions: [ChangedRegion] = []
+    var lastRefreshAt: Date?
+    var fileLastModifiedAt: Date?
+    var lastExternalChangeAt: Date?
+    var hasUnacknowledgedExternalChange: Bool = false
+    var unacknowledgedExternalChangeKind: ReaderExternalChangeKind = .modified
+
+    static let empty = DocumentContent()
+}
